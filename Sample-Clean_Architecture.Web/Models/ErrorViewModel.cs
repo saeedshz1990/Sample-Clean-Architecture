ce6b05485e1c63ed982d92e879ff1d83e2f838e0
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2023-10-17 21:10:36 +0330

    added two end point the project

---
+++ Sample-Clean_Architecture.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sample_Clean_Architecture.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
